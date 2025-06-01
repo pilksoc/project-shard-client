9d0302f20f0577b41e7c1249dffa28487e38eb43
Danny Piper <djpiper28@gmail.com>
2025-06-01 21:08:43 +0100

    feat: poll the websocket

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,40 @@
 #define SCREEN_WIDTH    640
 #define SCREEN_HEIGHT   480
 
+typedef struct state_t {
+    WebsocketClient *wsClient;
+    SDL_Window *window;
+} state_t;
+
+static int poll(state_t *state)
+{
+    SDL_Event e;
+    SDL_WaitEvent(&e);
+
+    if(e.type == SDL_QUIT) {
+        return 1;
+    }
+
+    if (state->wsClient != nullptr) {
+        auto resp = state->wsClient->Poll();
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+    SDL_RenderFillRect(renderer, &squareRect);
+    SDL_RenderPresent(renderer);
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     std::cerr << "Version: " << PSC_PROJECT_VERSION << std::endl;
     std::cerr << "OS: " << PSC_OS << std::endl;
     std::cerr << PSC_PROJECT_NAME << " - " PSC_PROJECT_VERSION << std::endl;
-    WebsocketClient websocketClient("http://project-shard.leonic.moe/ws");
+
+    state_t state = {};
+    state.wsClient = new WebsocketClient("http://project-shard.leonic.moe/ws");
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not be initialized!" << std::endl
@@ -20,75 +48,48 @@ int main(int argc, char **argv)
     }
 
     // Create window
-    SDL_Window *window = SDL_CreateWindow("Basic C++ SDL project",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT,
-                                          SDL_WINDOW_SHOWN);
+    state.window = SDL_CreateWindow("Basic C++ SDL project",
+                                    SDL_WINDOWPOS_UNDEFINED,
+                                    SDL_WINDOWPOS_UNDEFINED,
+                                    SCREEN_WIDTH, SCREEN_HEIGHT,
+                                    SDL_WINDOW_SHOWN);
     if(!window) {
         std::cout << "Window could not be created!" << std::endl
                   << "SDL_Error: " << SDL_GetError() << std::endl;
-    } else {
+        return 1;
         // Create renderer
         SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
         if(!renderer) {
             std::cout << "Renderer could not be created!" << std::endl
                       << "SDL_Error: " << SDL_GetError() << std::endl;
-        } else {
-            // Declare rect of square
-            SDL_Rect squareRect;
-
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-            // Square position: In the middle of the screen
-            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
-
-            // Event loop exit flag
-            bool quit = false;
-
-            // Event loop
-            while(!quit) {
-                SDL_Event e;
-
-                // Wait indefinitely for the next available event
-                SDL_WaitEvent(&e);
-
-                // User requests quit
-                if(e.type == SDL_QUIT) {
-                    quit = true;
-                }
-
-                // Initialize renderer color white for the background
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-                // Clear screen
-                SDL_RenderClear(renderer);
-
-                // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
-                // Draw filled square
-                SDL_RenderFillRect(renderer, &squareRect);
-
-                // Update screen
-                SDL_RenderPresent(renderer);
-            }
-
-            // Destroy renderer
-            SDL_DestroyRenderer(renderer);
         }
 
-        // Destroy window
-        SDL_DestroyWindow(window);
+        SDL_Rect squareRect;
+
+        squareRect.w = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+        squareRect.h = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+
+        squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
+        squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
+
+
+        // Event loop exit flag
+        bool quit = false;
+
+        // Event loop
+        while(!quit) {
+            quit = poll(&state);
+        }
+
+        SDL_DestroyRenderer(renderer);
     }
 
-    // Quit SDL
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
+    if (state.wsClient != nullptr) {
+        delete state.wsClient;
+    }
 
     return 0;
 }
